9292080c89ba7c6597a7999dec37387f139c02d1
adaichan <ll1322688810@gmail.com>
2021-01-02 20:08:17 +0800

    adaichan 2021.01.02 M

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-// shw2018 洪卫  add 2019.08.15
+// adaichan 2020.12.04 M
 var gulp = require("gulp");
 var debug = require("gulp-debug");
-var cleancss = require("gulp-clean-css"); //css压缩组件
-var uglify = require("gulp-uglify"); //js压缩组件
-var htmlmin = require("gulp-htmlmin"); //html压缩组件
-var htmlclean = require("gulp-htmlclean"); //html清理组件
-var imagemin = require("gulp-imagemin"); //图片压缩组件
-var changed = require("gulp-changed"); //文件更改校验组件
-var gulpif = require("gulp-if"); //任务 帮助调用组件
-var plumber = require("gulp-plumber"); //容错组件（发生错误不跳出任务，并报出错误内容）
-var isScriptAll = true; //是否处理所有文件，(true|处理所有文件)(false|只处理有更改的文件)
-var isDebug = true; //是否调试显示 编译通过的文件
+var cleancss = require("gulp-clean-css"); // css压缩组件
+var uglify = require("gulp-uglify"); // js压缩组件
+var htmlmin = require("gulp-htmlmin"); // html压缩组件
+var htmlclean = require("gulp-htmlclean"); // html清理组件
+var imagemin = require("gulp-imagemin"); // 图片压缩组件
+var changed = require("gulp-changed"); // 文件更改校验组件
+var gulpif = require("gulp-if"); // 任务帮助调用组件
+var plumber = require("gulp-plumber"); // 容错组件（发生错误不跳出任务，并报出错误内容）
+var isScriptAll = true; // 是否处理所有文件，(true|处理所有文件)(false|只处理有更改的文件)
+var isDebug = true; // 是否调试显示 编译通过的文件
 var gulpBabel = require("gulp-babel");
 var es2015Preset = require("babel-preset-es2015");
 var del = require("del");
@@ -23,7 +23,7 @@ gulp.task("clean", function() {
 });
 
 // 下面几个跟hexo有关的操作，主要通过hexo.call()去执行，注意return
-// 创建静态页面 （等同 hexo generate）
+// 创建静态页面（等同 hexo generate）
 gulp.task("generate", function() {
   return hexo.init().then(function() {
     return hexo
@@ -87,10 +87,10 @@ gulp.task("compressJs", function() {
 gulp.task("compressCss", function() {
   var option = {
     rebase: false,
-    //advanced: true,               //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+    //advanced: true, //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
     compatibility: "ie7" //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
-    //keepBreaks: true,             //类型：Boolean 默认：false [是否保留换行]
-    //keepSpecialComments: '*'      //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+    //keepBreaks: true, //类型：Boolean 默认：false [是否保留换行]
+    //keepSpecialComments: '*' //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
   };
   return gulp
     .src(["./public/**/*.css", "!./public/**/*.min.css"]) //排除的css
@@ -108,15 +108,15 @@ gulp.task("compressHtml", function() {
     unprotect: /<script [^>]*\btype="text\/x-handlebars-template"[\s\S]+?<\/script>/gi //特殊处理
   };
   var minOption = {
-    collapseWhitespace: true, //压缩HTML
-    collapseBooleanAttributes: true, //省略布尔属性的值  <input checked="true"/> ==> <input />
-    removeEmptyAttributes: true, //删除所有空格作属性值    <input id="" /> ==> <input />
-    removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
-    removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
-    removeComments: true, //清除HTML注释
-    minifyJS: true, //压缩页面JS
-    minifyCSS: true, //压缩页面CSS
-    minifyURLs: true //替换页面URL
+    collapseWhitespace: true, // 压缩HTML
+    collapseBooleanAttributes: true, // 省略布尔属性的值  <input checked="true"/> ==> <input />
+    removeEmptyAttributes: true, // 删除所有空格作属性值    <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true, // 删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true, // 删除<style>和<link>的type="text/css"
+    removeComments: true, // 清除HTML注释
+    minifyJS: true, // 压缩页面JS
+    minifyCSS: true, // 压缩页面CSS
+    minifyURLs: true // 替换页面URL
   };
   return gulp
     .src("./public/**/*.html")
@@ -130,10 +130,10 @@ gulp.task("compressHtml", function() {
 // 压缩 public/uploads 目录内图片
 gulp.task("compressImage", function() {
   var option = {
-    optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-    progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-    interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-    multipass: false //类型：Boolean 默认：false 多次优化svg直到完全优化
+    optimizationLevel: 5, // 类型：Number  默认：3  取值范围：0-7（优化等级）
+    progressive: true, // 类型：Boolean 默认：false 无损压缩jpg图片
+    interlaced: false, // 类型：Boolean 默认：false 隔行扫描gif进行渲染
+    multipass: false // 类型：Boolean 默认：false 多次优化svg直到完全优化
   };
   return gulp
     .src("./public/medias/**/*.*")
@@ -143,7 +143,7 @@ gulp.task("compressImage", function() {
     .pipe(imagemin(option))
     .pipe(gulp.dest("./public"));
 });
-// 执行顺序： 清除public目录 -> 产生原始博客内容 -> 执行压缩混淆 -> 部署到服务器
+// 执行顺序：清除public目录 -> 产生原始博客内容 -> 执行压缩混淆 -> 部署到服务器
 gulp.task(
   "build",
   gulp.series(
@@ -166,4 +166,4 @@ gulp.task(
     gulp.parallel("compressHtml", "compressCss", "compressImage", "compressJs")
   )
 );
-//Gulp4最大的一个改变就是gulp.task函数现在只支持两个参数，分别是任务名和运行任务的函数
+// Gulp4最大的一个改变就是gulp.task函数现在只支持两个参数，分别是任务名和运行任务的函数
